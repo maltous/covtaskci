602700e5209fdb7781a8a667db5005e6fe779017
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-17 12:21:41 +0300

    contaskci

---
+++ story_content/user.js
@@ -0,0 +1,55 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6M4B9Sdm2Ms":
+        Script1();
+        break;
+      case "62kWpzk9Q7i":
+        Script2();
+        break;
+      case "5dTzBOD8evx":
+        Script3();
+        break;
+      case "6PgTWnjw7mj":
+        Script4();
+        break;
+      case "5imq8tIYSzz":
+        Script5();
+        break;
+      case "6NYKtLDFNhG":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script5()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script6()
+{
+  window.print();
+}
+
